5eb1d30812574be1aaa15e02a6442a54fc0e3939
selxial <brilliantlma79@gmail.com>
2024-06-07 19:33:51 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "ROFL": "Tanggapan terhadap lelucon",
+            "SHEESH": "Sedikit ketidaksetujuan",
+            "CREEPY": "Menakutkan, tidak menyenangkan",
+            "AGGRO": "Untuk menjadi agresif/marah"
+            }
+            
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+
+if word in meme_dict.keys():
+    # Apa yang harus kita lakukan jika kata itu ditemukan?
+    print(meme_dict[word])
+else:
+    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+    print("Kata ini tidak ada di Kamus: ", "ASAP – As soon as possible", "KMK - Let me know")
